dcd44b5ff176d754a2378aa92a48b2c9835fc326
falahmajeed <abhiunni9656@gmail.com>
2025-01-25 19:23:05 +0530

    working until timer rolling

--- lib/wavectrl.dart
+++ lib/wavectrl.dart
@@ -6,17 +6,27 @@ import 'package:get/get.dart';
 class WaveController extends GetxController {
   final time1 = TextEditingController().obs;
   final sec = 0.obs;
+  final progressData = <double>[].obs;
 
   void settime() {
-    final sec = int.tryParse(time1.value.text);
-
-    startTimer(sec!);
+    // Parse the value from the TextField.
+    final parsedSec = int.tryParse(time1.value.text);
+    if (parsedSec == null || parsedSec <= 0) {
+      // Handle invalid or zero input.
+      sec.value = 0; // Update to trigger the "Enter a nonzero number" message.
+      debugPrint("Invalid input: Enter a nonzero number");
+    } else {
+      sec.value = parsedSec; // Update with the valid number.
+      startTimer(sec.value); // Start the timer with the entered time.
+      debugPrint("Got the time: ${sec.value}");
+    }
   }
 
   final progress = 0.0.obs;
   Timer? timer;
 
   void startTimer(int durationInSeconds) {
+    debugPrint("Start timer started");
     progress.value = 0.0; // Reset progress
     int elapsed = 0;
 
